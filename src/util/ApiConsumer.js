023f526e3223cab4650e27dca79a9e22c90bb7ff
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-04-26 01:51:20 +0200

    login function inplementation

--- src/util/ApiConsumer.js
+++ src/util/ApiConsumer.js
@@ -1,1 +1,19 @@
 const urlLocal = `http://127.0.0.1:5000`;
+
+//Object used to connect to local API and to fetch information
+const ApiConsumer = {
+
+    //sends email and password to API and saves token in a cookie
+    login : async (email, password) => {
+        try {
+            let response = await fetch('http://127.0.0.1:5000/users', 
+            { method: 'POST', body: { email: email, password: password }});
+            response = await response.json();
+            return response;
+        } catch (error) {
+            console.log(error);
+        }        
+    }
+}
+
+export default ApiConsumer
