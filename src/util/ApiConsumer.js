f72e93379204b54b51a2e3cb7fb3f8e36566b074
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-04-26 22:49:09 +0200

    login, register, to api

--- src/util/ApiConsumer.js
+++ src/util/ApiConsumer.js
@@ -1,9 +1,12 @@
+import Cookies from 'js-cookie';
+
 const urlLocal = `http://127.0.0.1:5000`;
 
 //Object used to connect to local API and to fetch information
 const ApiConsumer = {
 
     //sends email and password to API and saves token in a cookie
+    //Cookies.set('name', 'value')
     login : async (email, password) => {
         try {
             let response = await fetch('http://127.0.0.1:5000/users', 
@@ -12,8 +15,25 @@ const ApiConsumer = {
             return response;
         } catch (error) {
             console.log(error);
-        }        
-    }
+        }
+    },
+    register : async (name, lastname, email, age, password) => {
+        try {
+            let response = await fetch('http://127.0.0.1:5000/users', 
+            { method: 'POST', 
+            body: { 
+                name: name,
+                lastname: lastname,
+                email: email, 
+                age: age,
+                admin: false,
+                password: password }});
+            response = await response.json();
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }
 
 export default ApiConsumer
