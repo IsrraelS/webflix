e18e703b03cf250486ee0969e947af395fd49768
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-04-28 02:32:38 +0200

    create carousel in home
    carrusel de bootstrap  basico funcionando

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './Home.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import LordOfRings1 from "../home/img/LordOfRings1.jpeg"
+import LordOfRings2 from "../home/img/LordOfRings2.jpeg"
+import LordOfRings3 from "../home/img/LordOfRings3.jpeg"
 
 export function Home(){
 
@@ -8,7 +12,29 @@ export function Home(){
             <div className="cntMain">
                 <div className="boxMain">
                     <div className ="centralMain">
-                        <div className="central">Aqui va un carrusel</div>
+                        <div className="carouselHome">
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img src={ LordOfRings1 } className="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item">
+                                <img src={ LordOfRings2 } className="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item">
+                                <img src={ LordOfRings3 } className="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                            </div>
+                        </div>
                         <div className="mainFooter">Aqui va una fila de estrenos</div>
                     </div>
                 </div>  
