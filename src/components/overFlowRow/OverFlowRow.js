3c69beff7cf8175a8bcb8db292cb59933dbc00c9
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-05-01 20:48:17 +0200

    Update Home

--- src/components/overFlowRow/OverFlowRow.js
+++ src/components/overFlowRow/OverFlowRow.js
@@ -1,8 +1,6 @@
 import React from "react";
 import './OverFlowRow.scss'
 import 'bootstrap/dist/css/bootstrap.css'
-import theGodfather from './img/theGodfather.jpeg'
-import theGodfather2 from './img/theGodfather2.jpeg'
 import BoxMovie from "../boxMovie/BoxMovie";
 
 function OverFlowRow(){
