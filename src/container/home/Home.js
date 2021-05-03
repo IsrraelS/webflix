2620aa8bb74ace3ee4a6867711ae727fff0c20b0
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-05-03 03:51:51 +0200

    New rows overflow and resize styles
    se han implementado 4 nuevas filas, se ha redimensionado el carousel y el home

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -11,11 +11,29 @@ export function Home(){
             <div className="cntMain">
                 <div className="boxMain">
                     <div className ="centralMain">
+                        <h3>Top 10</h3>
                         <div className="carouselHome">
                             <Carousel></Carousel>
                         </div>
+                        <h5>Action</h5>
                         <div className="mainFooter">
                             <OverFlowRow genre="Action"/>
+                        </div>
+                        <h5>Comedy</h5>
+                        <div className="mainFooter">
+                            <OverFlowRow genre="Comedy"/>
+                        </div>
+                        <h5>Suspense</h5>
+                        <div className="mainFooter">
+                            <OverFlowRow genre="Suspense"/>
+                        </div>
+                        <h5>Terror</h5>
+                        <div className="mainFooter">
+                            <OverFlowRow genre="Terror"/>
+                        </div>
+                        <h5>Drama</h5>
+                        <div className="mainFooter">
+                            <OverFlowRow genre="Drama"/>
                         </div>
                     </div>
                 </div>
